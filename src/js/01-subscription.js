ad6d0d47fd2414d94606800728a314788c243ef5
Allovallo <30566134+Allovallo@users.noreply.github.com>
2023-10-12 21:47:41 +0300

    Files updated_er
    Files updated_er

--- src/js/01-subscription.js
+++ src/js/01-subscription.js
@@ -1,17 +1,28 @@
 import '../css/common.css';
+import BSN from 'bootstrap.native';
 
 const PROMPT_DELAY = 1000;
 const MAX_PROMPT_ATTEMPT = 3;
+let modal = new BSN.Modal(
+  '#myModal', // target selector
+  { // options object
+    backdrop: 'static', // we don't want to dismiss Modal when Modal or backdrop is the click event target
+    keyboard: false // we don't want to dismiss Modal on pressing [Esc] key
+  }
+);
+
+console.log(modal);
+modal.show();
 
 let promptCounter = 0;
 let hasSubscribed = false;
 
 const intevalId = setInterval(() => {
-if (promptCounter === MAX_PROMPT_ATTEMPT) {
-    console.log('Зупиняємо інтервал!');
-    clearInterval(intevalId);
-    return;
-}
+    if (promptCounter === MAX_PROMPT_ATTEMPT) {
+        console.log('Зупиняємо інтервал!');
+        clearInterval(intevalId);
+        return;
+    }
     console.log('Підпишісь на розсилку');
     promptCounter += 1;
     console.log(promptCounter);
