1f6536b248c185db71e7bc01df8fd9f862739860
Allovallo <30566134+Allovallo@users.noreply.github.com>
2023-10-16 18:12:37 +0300

    files updated
    files updated

--- src/js/01-subscription.js
+++ src/js/01-subscription.js
@@ -1,34 +1,35 @@
 import '../css/common.css';
-import BSN from 'bootstrap.native';
+// import BSN from 'bootstrap.native';
 
-const refs = {
-  modal: document.querySelector('#myModal'),
-  subscribeBtn: document.querySelector('button[data-subscribe]'),
-}
-const PROMPT_DELAY = 1000;
-const MAX_PROMPT_ATTEMPT = 3;
-let promptCounter = 0;
-let hasSubscribed = false;
-const modal = new BSN.Modal('myModal');
+// const refs = {
+//   modal: document.querySelector('#myModal'),
+//   subscribeBtn: document.querySelector('button[data-subscribe]'),
+// }
+// const PROMPT_DELAY = 1000;
+// const MAX_PROMPT_ATTEMPT = 3;
+// let promptCounter = 0;
+// let hasSubscribed = false;
+const modal = new BSN.Modal('#myModal');
 
-openModal();
+// openModal();
 
-refs.modal.addEventListener('hide.bs.modal', openModal);
-refs.subscribeBtn.addEventListener('click', onSubscribeBtnClick);
+// refs.modal.addEventListener('hide.bs.modal', openModal);
+// refs.subscribeBtn.addEventListener('click', onSubscribeBtnClick);
 
-function openModal() {
-  if (promptCounter === MAX_PROMPT_ATTEMPT || hasSubscribed) {
-      console.log('Максимальна кількість набридів або підписався!');
-      return;
-  }  
-  setTimeout(() => {
-    console.log('Відкриваємо набридалку');
-    modal.show();
-    promptCounter += 1;
-  }, PROMPT_DELAY);
-}
+// function openModal() {
+//   if (promptCounter === MAX_PROMPT_ATTEMPT || hasSubscribed) {
+//     console.log('Максимальна кількість набридів або підписався!');
+//     return;
+//   }
 
-function onSubscribeBtnClick() {
-  hasSubscribed = true;
-  modal.hide();
-}
+//   setTimeout(() => {
+//     console.log('Відкриваємо набридалку');
+//     modal.show();
+//     promptCounter += 1;
+//   }, PROMPT_DELAY);
+// }
+
+// function onSubscribeBtnClick() {
+//   hasSubscribed = true;
+//   modal.hide();
+// }
