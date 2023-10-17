a941be4026dad8465f7cc5eee33f9f88f36ec232
Allovallo <30566134+Allovallo@users.noreply.github.com>
2023-10-17 22:13:03 +0300

    files updated
    files updated

--- src/js/01-subscription.js
+++ src/js/01-subscription.js
@@ -1,33 +1,33 @@
 import '../css/common.css';
 // import BSN from 'bootstrap.native';
 
-// const refs = {
-//   modal: document.querySelector('#myModal'),
-//   subscribeBtn: document.querySelector('button[data-subscribe]'),
-// }
-// const PROMPT_DELAY = 1000;
-// const MAX_PROMPT_ATTEMPT = 3;
-// let promptCounter = 0;
-// let hasSubscribed = false;
+const refs = {
+  modal: document.querySelector('#myModal'),
+  subscribeBtn: document.querySelector('button[data-subscribe]'),
+}
+const PROMPT_DELAY = 1000;
+const MAX_PROMPT_ATTEMPTS = 3;
+let promptCounter = 0;
+let hasSubscribed = false;
 const modal = new BSN.Modal('#myModal');
 
-// openModal();
+openModal();
 
-// refs.modal.addEventListener('hide.bs.modal', openModal);
+refs.modal.addEventListener('hide.bs.modal', openModal);
 // refs.subscribeBtn.addEventListener('click', onSubscribeBtnClick);
 
-// function openModal() {
-//   if (promptCounter === MAX_PROMPT_ATTEMPT || hasSubscribed) {
-//     console.log('Максимальна кількість набридів або підписався!');
-//     return;
-//   }
+function openModal() {
+  if (promptCounter === MAX_PROMPT_ATTEMPTS || hasSubscribed) {
+    console.log('Максимальна кількість набридів або підписався!');
+    return;
+  }
 
-//   setTimeout(() => {
-//     console.log('Відкриваємо набридалку');
-//     modal.show();
-//     promptCounter += 1;
-//   }, PROMPT_DELAY);
-// }
+  setTimeout(() => {
+    console.log('Відкриваємо набридалку');
+    modal.show();
+    promptCounter += 1;
+  }, PROMPT_DELAY);
+}
 
 // function onSubscribeBtnClick() {
 //   hasSubscribed = true;
--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -4,21 +4,21 @@ const refs = {
     startBtn: document.querySelector('button[data-action-start]'),
     stopBtn: document.querySelector('button[data-action-stop]'),
     clockface: document.querySelector('.js-clockface'),
-};
+}
 
 class Timer {
     constructor({onTick}) {
         this.intervalId = null;
-        this.isActive = false;
+        this.isActive = false; 
         this.onTick = onTick;
 
         this.init();
     }
-
+    
     init() {
-        const time = this.getTimeComponents(0);
+        const time = this.getTimeComponenets(0);
         this.onTick(time);
-    }
+    };
 
     start() {
         if (this.isActive) {
@@ -31,28 +31,28 @@ class Timer {
         this.intervalId = setInterval(() => {
             const currentTime = Date.now();
             const deltaTime = currentTime - startTime;
-            const time = this.getTimeComponents(deltaTime);
+            const time = this.getTimeComponenets(deltaTime);
 
             this.onTick(time);
         }, 1000);
-    }
+    };
 
     stop() {
         clearInterval(this.intervalId);
         this.isActive = false;
-        const time = this.getTimeComponents(deltaTime);
+        const time = this.getTimeComponenets(0);
         this.onTick(time);
-    }
+    };
 
-    getTimeComponents(time) {
+    getTimeComponenets(time) {
         const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
         const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
         const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
 
         return { hours, mins, secs };
-    }
+    };
 
-    pad (value) {
+    pad(value) {
         return String(value).padStart(2, '0');
     }
 }
@@ -61,9 +61,9 @@ const timer = new Timer({
     onTick: updateClockface,
 });
 
-refs.startBtn.addEventListener('click', timer.start.bind(timer));
-refs.stopBtn.addEventListener('click', timer.stop.bind(timer));
+refs.startBtn.addEventListener('click', () => { timer.start() });
+refs.stopBtn.addEventListener('click', () => { timer.stop() });
 
-function updateClockface({hours, mins, secs}) {
-    refs.clockface.textContent = `${hours}:${mins}:${secs}`;
+function updateClockface({ hours, mins, secs }) {
+    refs.clockface.textContent = `${hours}:${mins}:${secs}`;    
 }
